2ce14274418d9ed5879d560b2de4dfb3181f2d30
naomiskevo <naomiskevo@gmsail.com>
2020-01-21 14:46:44 -0600

    Updates

---
+++ precache-manifest.435967fa694212072327e656a3375511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b1f8f7f52fbae49136c66e9bd5b71b",
+    "url": "/assessment-4-circle-selector/index.html"
+  },
+  {
+    "revision": "0600691a6aff39d3ecf7",
+    "url": "/assessment-4-circle-selector/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "11c337bd83376470855a",
+    "url": "/assessment-4-circle-selector/static/js/2.ec0babb7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/assessment-4-circle-selector/static/js/2.ec0babb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0600691a6aff39d3ecf7",
+    "url": "/assessment-4-circle-selector/static/js/main.e927a536.chunk.js"
+  },
+  {
+    "revision": "01df315cde99e7668246",
+    "url": "/assessment-4-circle-selector/static/js/runtime-main.09569dc3.js"
+  }
+]);
